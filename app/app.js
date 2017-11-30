129af2290392983f1884a82bb82510027705ed88
Moe B <moeb59@gmail.com>
2017-11-30 16:36:01 -0500

    Using $routeProvider to configure routes.

--- app/app.js
+++ app/app.js
@@ -5,4 +5,14 @@
 
     var app = angular.module('customersApp', ['ngRoute']);
 
+    app.config(function ($routeProvider) {
+
+        $routeProvider
+            .when('/', {
+                controller: 'CustomersController',
+                templateUrl: 'app/views/customers.html'
+            })
+            .otherwise({ redirectTo: '/'});
+    });
+
 }());
